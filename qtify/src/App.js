1c8fdf97686c14a89c50e8f9738d68d7652e74b9
prajwalmj <prajwaljagadeesh2001@gmail.com>
2024-01-28 01:11:55 +0530

    hero

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from "./components/Hero/Hero"
 
 function App() {
   return (
-    <div><Navbar/>
+    <div>
+      <Navbar/>
+      <Hero/>
     </div>
   );
 }
